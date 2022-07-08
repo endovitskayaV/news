78081485605236a0abae45be74eee198b0279755
Valentina Endovitskaya <evvEndovitskaya@yandex.ru>
2022-07-08 14:39:18 +0300

    splits investigating

--- src/final.py
+++ src/final.py
@@ -9,7 +9,7 @@ from pandas import Series, DataFrame
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import r2_score
-from sklearn.model_selection import RandomizedSearchCV, cross_validate, TimeSeriesSplit
+from sklearn.model_selection import RandomizedSearchCV
 from sklearn.model_selection import train_test_split
 
 from settings import LOGGING_PATH, DATA_PATH
@@ -117,6 +117,7 @@ df_train['day'] = pd.to_datetime(df_train['publish_date']).dt.strftime("%d").ast
 df_train['month'] = pd.to_datetime(df_train['publish_date']).dt.strftime("%m").astype(int)
 df_train['hour'] = pd.to_datetime(df_train['publish_date']).dt.strftime("%H").astype(int)
 df_train['minute'] = pd.to_datetime(df_train['publish_date']).dt.strftime("%M").astype(int)
+df_train['date'] = df_train['publish_date'].apply(lambda _date: _date.date())
 
 holiday_dates = pd.read_csv(DATA_PATH / 'holidays.csv', sep=';')
 dates = holiday_dates['date'].apply(lambda _date: datetime.strptime(_date, "%Y-%m-%d").date())
@@ -126,28 +127,51 @@ df_train['is_holiday'] = df_train['is_holiday'].astype(int)
 df_train = df_train.apply(lambda row: weekend_fun(row), axis=1)
 df_train['is_weekend'] = df_train['is_weekend'].astype(int)
 
-categs={
+categs = {
     'политика': ['5409f11ce063da9c8b588a12'],
     'общество': ['5433e5decbb20f277b20eca9'],
-'бизнес_финансы': ['540d5eafcbb20f2524fc0509', '5409f11ce063da9c8b588a18'],
-'экономика': ['5409f11ce063da9c8b588a13', ],
-'медиа и технологии': ['540d5ecacbb20f2524fc050a'],
-        }
-df_dict = {category_name: df_train[df_train['category'].isin(category_ids)] for category_name, category_ids in categs.items()}
+    'бизнес_финансы': ['540d5eafcbb20f2524fc0509', '5409f11ce063da9c8b588a18'],
+    'экономика': ['5409f11ce063da9c8b588a13', ],
+    'медиа и технологии': ['540d5ecacbb20f2524fc050a'],
+}
+df_dict = {category_name: df_train[df_train['category'].isin(category_ids)] for category_name, category_ids in
+           categs.items()}
+
+
+def split(df):
+    df_train = DataFrame()
+    df_test = DataFrame()
+    groups = df.groupby('date')
+    for name, group in groups:
+        if group.shape[0] < 3:
+            df_train = pd.concat([group, df_train])
+        else:
+            group_train = group.sample(frac=0.8)
+            group_test = group.loc[~group.index.isin(group_train.index)]
+            df_train = pd.concat([group_train, df_train])
+            df_test = pd.concat([group_test, df_test])
+
+    return df_train, df_test
 
 
 for category, df_train in df_dict.items():
     logger.log(msg="category " + category, level=logging.getLevelName("WARNING"))
-    X = df_train.drop(['is_weekend', 'views', 'category', 'depth', "full_reads_percent", "publish_date", "session", "document_id"], axis=1)
-    y = df_train[["views", "depth", "full_reads_percent"]]
 
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+    df_train, df_test = split(df_train)
+    x_cols_drop = ['is_weekend', 'views', 'category', 'depth', "full_reads_percent", "publish_date", "session",
+                   "document_id", 'date']
+    y_cols = ["views", "depth", "full_reads_percent"]
+
+    X_train = df_train.drop(x_cols_drop, axis=1)
+    y_train = df_train[y_cols]
+    X_test = df_test.drop(x_cols_drop, axis=1)
+    y_test = df_test[y_cols]
 
     vectorizer = TfidfVectorizer(tokenizer=identity, lowercase=False)
     # vectorizer = loads(DATA_PATH / "views_vectorizer.pickle")
-    train_texts = vectorizer.fit_transform(X['title'])
-    test_texts = vectorizer.transform(X['title'])
-    dump(DATA_PATH / (category+"vectorizer.pickle"), vectorizer)
+    train_texts = vectorizer.fit_transform(X_train['title'])
+    test_texts = vectorizer.transform(X_test['title'])
+    dump(DATA_PATH / (category + "vectorizer.pickle"), vectorizer)
 
     train_texts_arr = train_texts.toarray()
     train_texts_df = pd.DataFrame(train_texts_arr)
@@ -156,15 +180,15 @@ for category, df_train in df_dict.items():
     train_texts_df.rename(lambda col_name: "text_" + str(col_name), axis='columns', inplace=True)
     test_texts_df.rename(lambda col_name: "text_" + str(col_name), axis='columns', inplace=True)
 
-    X = X.reset_index()
-    # X_test = X_test.reset_index()
-    y = y.reset_index()
-    # y_test = y_test.reset_index()
+    X_train = X_train.reset_index()
+    X_test = X_test.reset_index()
+    y_train = y_train.reset_index()
+    y_test = y_test.reset_index()
 
-    X = X.merge(train_texts_df, left_index=True, right_index=True)
-    # X_test = X_test.merge(test_texts_df, left_index=True, right_index=True)
-    X.drop(['title', 'index'], axis=1, inplace=True)
-    # X_test.drop(['title', 'index'], axis=1, inplace=True)
+    X_train = X_train.merge(train_texts_df, left_index=True, right_index=True)
+    X_test = X_test.merge(test_texts_df, left_index=True, right_index=True)
+    X_train.drop(['title', 'index'], axis=1, inplace=True)
+    X_test.drop(['title', 'index'], axis=1, inplace=True)
 
     feature_array = np.array(vectorizer.get_feature_names_out())
     tfidf_sorting = np.argsort(train_texts_arr).flatten()[::-1]
@@ -179,9 +203,9 @@ for category, df_train in df_dict.items():
     # score = calculate_score(y_test, search.predict(X_test), ['views'])
     # print(score)
 
-    def train_score(index: int, y_cols: List[str], X, y):
-        y_train = y[y_cols]
-        # y_test = y_test[y_cols]
+    def train_score(index: int, y_cols: List[str], X_train, X_test, y_train, y_test):
+        y_train = y_train[y_cols]
+        y_test = y_test[y_cols]
 
         if y_train.shape[1] == 1:
             y_train = y_train.values.ravel()
@@ -189,8 +213,8 @@ for category, df_train in df_dict.items():
         logger.log(msg="y_cols " + str(y_cols), level=logging.getLevelName("WARNING"))
 
         for n_components_rate in [0]:
-            X_train_new = X.copy()
-            # X_test_new = X_test.copy()
+            X_train_new = X_train.copy()
+            X_test_new = X_test.copy()
             # for n_components_rate in [0.6, 0.8]:
             #     n_components = int(n_components_rate * X_train.shape[1])
             #     logger.log(msg="n_components " + str(n_components), level=logging.getLevelName("WARNING"))
@@ -214,30 +238,27 @@ for category, df_train in df_dict.items():
             #                                cv=3, verbose=2, random_state=42, n_jobs=-1,
             #                                return_train_score=True)
             # search = rf_random.fit(X_train_new, y_train)
-            p = {'n_estimators': 500, 'max_depth': 20}
+            p = {'n_estimators': 500, 'max_features': 0.8, 'max_depth': 20}
             search = RandomForestRegressor(**p)
-            cv=TimeSeriesSplit(n_splits=5)
-            scores = cross_validate(search, X_train_new, y_train, cv=cv, scoring='r2',   return_train_score=True)
-            logger.log(msg="scores " + str(scores), level=logging.getLevelName("WARNING"))
-            # search.fit(X_train_new, y_train)
-            # dump(DATA_PATH / (str(index) + "reg.pickle"), search)
+            search.fit(X_train_new, y_train)
+            dump(DATA_PATH / (str(index) + "reg.pickle"), search)
             # logger.log(msg="params " + str(search.best_params_), level=logging.getLevelName("WARNING"))
             # logger.log(msg="best_score_ " + str(search.best_score_), level=logging.getLevelName("WARNING"))
-            # logger.log(msg="train score r2 " + str(r2_score(y_train, search.predict(X_train_new))),
-            #            level=logging.getLevelName("WARNING"))
-            # logger.log(msg="train score " + str(search.score(X_train_new, y_train)),
-            #            level=logging.getLevelName("WARNING"))
-            # logger.log(msg="test score " + str(search.score(X_test_new, y_test)), level=logging.getLevelName("WARNING"))
-            # logger.log(msg="test score r2 " + str(r2_score(y_test, search.predict(X_test_new))),
-            #            level=logging.getLevelName("WARNING"))
-            # score = calculate_score(y_test, search.predict(X_test_new), y_cols)
-            # logger.log(msg="test score custom " + str(score), level=logging.getLevelName("WARNING"))
+            logger.log(msg="train score r2 " + str(r2_score(y_train, search.predict(X_train_new))),
+                       level=logging.getLevelName("WARNING"))
+            logger.log(msg="train score " + str(search.score(X_train_new, y_train)),
+                       level=logging.getLevelName("WARNING"))
+            logger.log(msg="test score " + str(search.score(X_test_new, y_test)), level=logging.getLevelName("WARNING"))
+            logger.log(msg="test score r2 " + str(r2_score(y_test, search.predict(X_test_new))),
+                       level=logging.getLevelName("WARNING"))
+            score = calculate_score(y_test, search.predict(X_test_new), y_cols)
+            logger.log(msg="test score custom " + str(score), level=logging.getLevelName("WARNING"))
             logger.log(msg="\n", level=logging.getLevelName("WARNING"))
 
 
     l1 = [["views"], ["depth"], ["full_reads_percent"], ["views", "depth", "full_reads_percent"]]
-    for index, y_cols in enumerate(l1):
-        train_score(index, y_cols, X, y)
+    for index, y_cols in enumerate([["views"]]):
+        train_score(index, y_cols, X_train.copy(), X_test.copy(), y_train.copy(), y_test.copy())
         logger.log(msg="\n", level=logging.getLevelName("WARNING"))
 
 logger.log(msg="\n", level=logging.getLevelName("WARNING"))
