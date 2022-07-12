25aba4c6f904f3867596efa9f27e7e1ca31d7e4f
Valentina Endovitskaya <evvEndovitskaya@yandex.ru>
2022-07-12 19:09:13 +0300

    viwes

--- src/transform_views_test.py
+++ src/transform_views_test.py
@@ -1,4 +1,5 @@
 import ast
+import json
 import logging
 import urllib
 from datetime import datetime, timedelta
@@ -8,9 +9,7 @@ import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
 from pandas import Series, DataFrame
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score
-from sklearn.preprocessing import OneHotEncoder, MultiLabelBinarizer
 
 from settings import LOGGING_PATH, DATA_PATH, RAW_PATH
 from src.utils import loads
@@ -134,22 +133,20 @@ def curs_fun(row: Series, dollar_df: DataFrame) -> Series:
 
 def len_fun(row: Series) -> Series:
     id = row['document_id'][0:24]
-    content = None
-    text=""
+    views = -1
     try:
         content = urllib.request.urlopen("https://www.rbc.ru/rbcfreenews/" + id).read()
+        soup = BeautifulSoup(content, 'lxml')
+        div = soup.select_one('.rbcslider__slide')
+        paths = div.attrs['data-shorturl'].split('/')
+        idd = paths[-1]
+        content = urllib.request.urlopen("https://www.rbc.ru/redir/stat/" + idd).read()
+        response = json.loads(content)
+        views = response['show']
     except Exception as e:
         logger.log(msg=e, level=logging.getLevelName("ERROR"))
 
-    if content:
-        soup = BeautifulSoup(content, 'lxml')
-        text = soup.select_one('.article__text_free')
-        text = text.text
-    else:
-        logger.log(msg="no content", level=logging.getLevelName("WARNING"))
-
-    row['text'] = text
-    row['text_length'] = len(text)
+    row['real_views'] = views
     return row
 
 
@@ -165,11 +162,13 @@ logger.addHandler(general_fh)
 #
 # # transform data
 #
-# df_test = pd.read_csv(RAW_PATH / "test.csv", parse_dates=['publish_date'])
-#
-# df_test.sort_values('publish_date', inplace=True)
-# df_test['Time'] = np.arange(len(df_test.index))
-# df_test = df_test.reset_index(drop=True)
+df_test = pd.read_csv(RAW_PATH / "test.csv", parse_dates=['publish_date'])
+
+df_test.sort_values('publish_date', inplace=True)
+df_test['Time'] = np.arange(len(df_test.index))
+df_test = df_test.reset_index(drop=True)
+df_test = df_test.apply(lambda row: len_fun(row), axis=1)
+
 #
 # category_encoder = loads(DATA_PATH / "category_encoder.pickle")
 # categs = category_encoder.transform(df_test[['category']]).toarray()
@@ -220,13 +219,13 @@ logger.addHandler(general_fh)
 df_test_prepared = pd.read_csv(DATA_PATH / "df_test_prepared_v.csv")
 df_test = pd.read_csv(DATA_PATH / "df_test_depth_pred.csv")
 
-#x_cols_drop = ["publish_date", "session", "document_id", 'date', 'title','text']
-#X=df_test.drop(x_cols_drop, axis=1)
+# x_cols_drop = ["publish_date", "session", "document_id", 'date', 'title','text']
+# X=df_test.drop(x_cols_drop, axis=1)
 
 search = loads(DATA_PATH / "0reg_authors_short_full_rp056.pickle")
 X = df_test[search.feature_names_in_]
 views = search.predict(X)
 views_df = X.merge(pd.Series(views).rename("full_reads_percent"), left_index=True, right_index=True)
 views_df['document_id'] = df_test_prepared['document_id']
-views_df=views_df[["document_id", "views", "depth", "full_reads_percent"]]
+views_df = views_df[["document_id", "views", "depth", "full_reads_percent"]]
 views_df.to_csv(DATA_PATH / "df_test_full_reads_percent_pred.csv", index=False)
